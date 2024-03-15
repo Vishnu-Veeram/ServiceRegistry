cb1e36b9100c22c21ff74844e53ea4139b63f96c
Vishnu Vardhan Reddy <vishnuvardhanreddyveeram@gmail.com>
2024-03-15 09:15:31 +0530

    Initial commit

---
+++ src/test/java/com/vicky/ServiceRegistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vicky.ServiceRegistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
